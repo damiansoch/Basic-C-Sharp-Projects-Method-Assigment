de8d701ef7ffcc0dc644b903fac2a966bc0724d5
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-15 10:44:20 +0100

    Add project files.

---
+++ Method Assigment/MathOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Assigment
+{
+    public class MathOperations
+    {
+        public static void Multi(int number)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            int result = number * randomNumber;
+
+            Console.WriteLine(number + " multiplied by " + randomNumber + " equals " + result + "\n");
+
+            Console.ReadLine();
+        }
+
+        public static void Div(int number)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 10);
+            int result = number / randomNumber;
+            int remainder = number % randomNumber;
+
+            Console.WriteLine(number + " divided by " + randomNumber + " equals " + result + "\n and the remainder is: " + remainder);
+
+            Console.ReadLine();
+
+
+        }
+
+        public static void SquareRoot(int number)
+        {
+            double result = Math.Sqrt(number);
+
+            Console.WriteLine("The Square root of " + number + " is " + result);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method Assigment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Assigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program will nake a user number,and than \n 1) multiply it by random number (0-100) \n 2) divide it by random number (1-10) and aldo will print the remainder  \n 3) gives the square root of the provided number\n");
+            Console.WriteLine("Please write whole number (0-100)");
+            int userImput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            MathOperations.Multi(userImput);
+            MathOperations.Div(userImput);
+            MathOperations.SquareRoot(userImput);
+        }
+
+    }
+}
