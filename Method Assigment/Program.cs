90fe40f7be0c5e84d12253403c2d4a0383fa4315
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-15 11:06:11 +0100

    fixed typo

--- Method Assigment/Program.cs
+++ Method Assigment/Program.cs
@@ -8,7 +8,7 @@ namespace Method_Assigment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Program will nake a user number,and than \n 1) multiply it by random number (0-100) \n 2) divide it by random number (1-10) and aldo will print the remainder  \n 3) gives the square root of the provided number \n " +
+            Console.WriteLine("Program will take a user number, and than \n 1) multiply it by random number (0-100) \n 2) divide it by random number (1-10) and aldo will print the remainder  \n 3) gives the square root of the provided number \n " +
                 "The program will print the results inside the function, as well as the returned values in the main program");
             Console.WriteLine("Please write whole number (0-100)");
             int userImput = Convert.ToInt32(Console.ReadLine());
